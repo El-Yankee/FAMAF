a3def5819df68e5c6fe4ac0ae39d5b77adcf79d0
El Yankee <114041407+El-Yankee@users.noreply.github.com>
2023-05-04 23:38:55 -0300

    Add files via upload

--- Algoritmos y Estructuras de Datos II/Parciales/Modelos Parcial 1/Tema E/kickstart (Tema E)/array_helpers.c
+++ Algoritmos y Estructuras de Datos II/Parciales/Modelos Parcial 1/Tema E/kickstart (Tema E)/array_helpers.c
@@ -10,16 +10,38 @@
 
 static const int EXPECTED_DIM_VALUE = 2;
 
-static const char* CITY_NAMES[CITIES] = {
-    "Cordoba", "Rosario", "Posadas", "Tilcara", "Bariloche"};
+static const char* CITY_NAMES[CITIES] = {"Cordoba", "Rosario", "Posadas", "Tilcara", "Bariloche"};
 static const char* SEASON_NAMES[SEASONS] = {"low", "high"};
 
-void array_dump(BakeryProductTable a)
-{
-    for (unsigned int city = 0u; city < CITIES; ++city)
-    {
-        for (unsigned int season = 0u; season < SEASONS; ++season)
-        {
+static void init_array(unsigned int a[CITIES * SEASONS]) {
+
+    for(unsigned int i = 0u; i < CITIES * SEASONS; i = i + 1) {
+
+        a[i] = CITIES;   // cualquier dato que no pertencezca a [0,4]
+    }
+}
+
+static int array_value_count(unsigned int a[CITIES * SEASONS], unsigned int value) {
+
+    unsigned int count = 0u;
+
+    for (unsigned int i = 0u; i < CITIES * SEASONS; i = i + 1) {
+
+    	if (a[i] == value) {
+
+            count = count + 1;
+        }
+    }
+
+    return count;
+}
+
+void array_dump(BakeryProductTable a) {
+
+    for (unsigned int city = 0u; city < CITIES; ++city) {
+
+        for (unsigned int season = 0u; season < SEASONS; ++season) {
+
             fprintf(stdout, "%s in %s season: flour (%u,%u) Yeast (%u,%u) Butter (%u,%u) Sales value %u",
                     CITY_NAMES[city], SEASON_NAMES[season], a[city][season].flour_cant,
                     a[city][season].flour_price, a[city][season].yeast_cant,
@@ -73,6 +95,8 @@ void array_from_file(BakeryProductTable array, const char* filepath) {
     }
 
     int i = 0;
+    unsigned int a[CITIES * SEASONS];
+    init_array(a);
 
     while (i < SEASONS * CITIES && !feof(file)) {
 
@@ -94,9 +118,25 @@ void array_from_file(BakeryProductTable array, const char* filepath) {
 
         BakeryProduct product = bakery_product_from_file(file);
 
+        a[i] = code;
+
+        if(array_value_count(a, code) > EXPECTED_DIM_VALUE) {	
+
+            fprintf(stderr, "More than 2 data for a city.\n");
+            exit(EXIT_FAILURE);
+        }
+
         array[city][season] = product;
 
         ++i;
     }
+
+    if (i < SEASONS * CITIES){
+
+        fprintf(stderr, "Missing info for cities.\n");
+        exit(EXIT_FAILURE);
+
+    }
+
     fclose(file);
 }
