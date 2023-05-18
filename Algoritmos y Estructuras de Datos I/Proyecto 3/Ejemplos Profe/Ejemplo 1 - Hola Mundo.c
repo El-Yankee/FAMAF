afdd1ac2448a7f406a7717db6590f6973a6605d7
El Yankee <114041407+El-Yankee@users.noreply.github.com>
2023-05-18 12:22:40 -0300

    Delete Algoritmos y Estructuras de Datos I directory

--- Algoritmos y Estructuras de Datos I/Proyecto 3/Ejemplos Profe/Ejemplo 1 - Hola Mundo.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>        /* inclusión biblioteca estandar */
-
-int main(void)            /* devuelve un int, sin argumentos */ 
- {
-    printf("Hello World!\n");
-
-    return 0;             /* resultado de la función */
- }
